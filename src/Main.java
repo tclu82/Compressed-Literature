e2ddbacc77837bf118223915e9e1348f66f78148
Zac <tclu82@uw.edu>
2016-06-23 20:29:31 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -46,44 +46,44 @@ public class Main {
             StringBuilder message = new StringBuilder();
             int c;
 
-			/** Read characters from the file into a string. */
-			while ((c = input.read()) != -1) {
-				message.append((char)c);
-			}
-			input.close();
-			CodingTree ct = new CodingTree(message.toString());
-			
-			// http://goo.gl/iCKWM2
-			output1.write(ct.myCodes.toString().getBytes("UTF-8"));
-			output1.close();
+            /** Read characters from the file into a string. */
+            while ((c = input.read()) != -1) {
+                message.append((char)c);
+            }
+            input.close();
+            CodingTree ct = new CodingTree(message.toString());
 
-			// https://goo.gl/bUOUHl
-			BitSet bs = new BitSet(ct.myBits.length());
-		
-			/** If myBits is 1, flip the bit of BitSet. */
-			for (int j = 0; j < ct.myBits.length(); j++) {
-				
-				if (ct.myBits.charAt(j) == '1') bs.flip(j);
-			}
-			
-			/** Use bit array. */
-			byte[] byteArray = bs.toByteArray();
-			output2.write(byteArray);
-			output2.close();
-			
-			/** File size for output. */
-			double original = (int) Files.size(Paths.get(THE_BOOK));
-			double compressed = (int) Files.size(Paths.get("compressed.txt"));
-	
-			/** How long this program runs. */
-			long endTime = System.currentTimeMillis();
-			long duration = endTime - startTime;
+            // http://goo.gl/iCKWM2
+            output1.write(ct.myCodes.toString().getBytes("UTF-8"));
+            output1.close();
 
-			/** Output display. */
-			System.out.println("Uncompressed file size: " + (int) original + " bytes");
-			System.out.println("Compressed file size: " + (int) compressed + " bytes");
-			System.out.printf("Compression ratio: %.2f", compressed * 100 / original);
-			System.out.println("%\nRunning Time: " + duration + " milliseconds");
+            // https://goo.gl/bUOUHl
+            BitSet bs = new BitSet(ct.myBits.length());
+
+            /** If myBits is 1, flip the bit of BitSet. */
+            for (int j = 0; j < ct.myBits.length(); j++) {
+
+                if (ct.myBits.charAt(j) == '1') bs.flip(j);
+            }
+
+            /** Use bit array. */
+            byte[] byteArray = bs.toByteArray();
+            output2.write(byteArray);
+            output2.close();
+
+            /** File size for output. */
+            double original = (int) Files.size(Paths.get(THE_BOOK));
+            double compressed = (int) Files.size(Paths.get("compressed.txt"));
+
+            /** How long this program runs. */
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+
+            /** Output display. */
+            System.out.println("Uncompressed file size: " + (int) original + " bytes");
+            System.out.println("Compressed file size: " + (int) compressed + " bytes");
+            System.out.printf("Compression ratio: %.2f", compressed * 100 / original);
+            System.out.println("%\nRunning Time: " + duration + " milliseconds");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
